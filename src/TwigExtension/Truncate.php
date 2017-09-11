a3b43d3ea59a9ba8348c0356339057b041cd3423
Greg Aiello <gregoryaiello693@gmail.com>
2017-09-11 16:50:15 -0400

    Initial commit.

---
+++ src/TwigExtension/Truncate.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\truncate\TwigExtension;
+
+use Drupal\Component\Utility\Unicode;
+
+/**
+ * Class DefaultService.
+ *
+ * @package Drupal\truncate
+ */
+class Truncate extends \Twig_Extension {
+
+    /**
+     *
+     */
+    public function getName() {
+        return 'truncate.twig_extension';
+    }
+
+    /**
+     * {@inheritdoc}
+     * This function must return the name of the extension. It must be unique.
+     */
+    public function getFilters() {
+        return [
+            new \Twig_SimpleFilter('truncate', array($this, 'truncate'))
+        ];
+   }
+
+    /**
+     * Wrapper for Drupal\Component\Utility\Unicode::truncate()
+     */
+    public function truncate($string, $maxLength, $wordsafe = TRUE, $addEllipsis = TRUE, $minWordsafeLength = 1) {
+        return Unicode::truncate($string, $maxLength, $wordsafe, $addEllipsis, $minWordsafeLength);
+    }
+
+}
